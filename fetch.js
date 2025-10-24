8dbdd40a2f6b030cbe8884c8f756cd2a22357bfb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-24 10:25:24 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -76,7 +76,7 @@ data: [DONE]
 `,{headers:{'content-type':'text/event-stream'}});
                   console.log(res);
                 }
-                throw new Error('asdf');
+                //throw new Error('asdf');
                 return revealHeaders(res);
               }catch(e){
                 return new Response(`data: {"id":"1","action":"success","created":'+new Date().getTime()+',"model":"gpt-5-mini-2025-08-07","role":"assistant","message":"'+String(e?.message??e)+'"}
