4fdf7be6e76c7855904eb8b0f8a8d83a5989c692
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-24 10:58:40 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -57,6 +57,10 @@
                 const _fetch = globalThis.fetch;
                 globalThis.fetch = extend(async function fetch(...args) {
                                 const url = String(args[0]?.url ?? args[0]);
+                                if(url.includes('/duckchat/v1/status')){
+                                        const req = new Request(...args);
+                                        args = [`${req.url}?time=${new Date().getTime}`,req];
+                                }
                                 if (['improving.llm.patrickring.net', 'quack.llm.patrickring.net', 'privacy-pro-eligible.json'].some(x => url.includes(x))) {
                                     return revealHeaders(new Response('{}'));
                                 }
