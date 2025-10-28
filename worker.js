7cffaeae0ca01beac3e58163084b0c715c3a14e7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-28 11:34:50 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,35 @@
+import { importModule } from './runner.js';
+
+const workerURL = `https://raw.githubusercontent.com/Patrick-ring-motive/venusaur/refs/heads/main/venusaur.js?${new Date().getTime()}`;
+const isPromise = x => x instanceof Promise || typeof x?.then === 'function' || x?.constructor?.name === 'Promise';
+
+
+let init;
+export default {
+  async fetch(request, env, ctx) {
+    ctx.passThroughOnException();
+    try{   
+      if(!init){
+        init = importModule(workerURL);
+      }
+      if(isPromise(init)){
+        init = await init;
+      }
+      console.log(init);
+      const { onRequest } = init;
+      const res = await onRequest(...arguments);
+      setTimeout(()=>{
+        if(!isPromise(init)){
+          init = null;
+        }
+      },5000);
+      return res;
+    }catch(e){
+      console.warn(e,...arguments);
+      return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
+        status : 569,
+        statusText:e?.message
+      });
+    }
+  },
+};
