0b91d65eb654397d54bba0371f6fc3aa9bab5924
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-11-08 10:36:27 -0600

    Create hash.js

---
+++ vector/hash.js
@@ -0,0 +1,11 @@
+const encoder = new TextEncoder();
+const encode = encoder.encode.bind(encoder);
+const decoder = new TextDecoder();
+const decode = decoder.decode.bind(decoder);
+
+
+async function sha256(text) {
+  const data = encode(text);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  return Array.from(new Uint8Array(hashBuffer);
+}
